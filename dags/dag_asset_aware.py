6186ab1f63e38ab1bdc775962313d1f0ea4a57aa
leovnoliveira <consultoria1.ceo@gmail.com>
2026-05-28 22:07:00 -0400

    refactor: add dev env on dag

--- dags/dag_asset_aware.py
+++ dags/dag_asset_aware.py
@@ -1,8 +1,25 @@
 from airflow.sdk import Asset, dag, task 
+from dotenv import load_dotenv
+from airflow.model import Variable
+import os 
+
+
+load_dotenv() 
+
+dbt_env = Variable.get("dbt_env", default_var="dev").lower()
+
+if dbt_env not in ("dev", "prod"):
+    raise ValueError(f"dbt_env inválido: {dbt_env!r}, use  'dev' ou 'prod'")
+
+asset_name_dev = f"postgres://{os.getenv('DOCKER_HOST')}:{os.getenv('DOCKER_PORT')}/postgres/public/stg_cadastros_2"
+asset_name_prod = f"postgres://{os.getenv('ASTRO_HOST')}:{os.getenv('ASTRO_PORT')}/railway/public/stg_cadastros_2"
+
+asset_name = asset_name_dev if dbt_env == "dev" else asset_name_prod
+
 
 asset_tabela = Asset(
-    name='postgres://shortline.proxy.rlwy.net:40123/railway/public/mart_metricas_clientes',
-    uri='postgres://shortline.proxy.rlwy.net:40123/railway/public/mart_metricas_clientes',
+    name=asset_name,
+    uri=asset_name,
     group='asset'
 )
 
